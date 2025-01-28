9cd4f056972fccb37102dde9b4ef231d189d4b5c
davidreyesga <jtyuar@gmail.com>
2025-01-28 15:13:54 -0500

    date picker removed

--- src/app/favorites/favorites.component.ts
+++ src/app/favorites/favorites.component.ts
@@ -3,22 +3,11 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FavoritesService } from '../services/favorites.service';
 
-// Importaciones de Angular Material
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatNativeDateModule } from '@angular/material/core';
-
 @Component({
   selector: 'app-favorites',
   standalone: true,
-  // Agregamos aquí los módulos necesarios para el Datepicker
   imports: [
-    CommonModule,
-    MatDatepickerModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatNativeDateModule
+    CommonModule
   ],
   templateUrl: './favorites.component.html',
   styleUrls: ['./favorites.component.css'],
